3445b4e9e813493409637a438b65850fd4426338
GandhamNandini <51950142+GandhamNandini@users.noreply.github.com>
2019-07-04 11:18:28 +0530

    Update CountOfCharacters.java

--- src/main/java/com/stackroute/pe4/CountOfCharacters.java
+++ src/main/java/com/stackroute/pe4/CountOfCharacters.java
@@ -4,6 +4,7 @@ public class CountOfCharacters {
     public int Count(String s,String n)
     {
         //Count of alphabet "a" in the given string
+
         int finalcount=s.length()-s.replaceAll(n,"").length();
 
         return finalcount;
